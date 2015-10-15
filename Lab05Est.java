40f7cbb71c8caea62024908c109427ad2041d30e
5jw <jenerictech@gmail.com>
2015-10-15 11:51:59 -0400

    Initial

---
+++ Lab05Est.java
@@ -0,0 +1,19 @@
+// Lab05Est.java
+// The Speeding Ticket Program
+// This is the student, starting version of Lab 05E.
+
+
+public class Lab05Est
+{
+	public static void main(String args[])
+	{
+		System.out.println("Lab 05E");
+		System.out.println();
+
+
+
+
+
+		System.out.println();
+	}
+}
